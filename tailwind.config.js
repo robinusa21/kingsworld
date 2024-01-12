f7b5bea9de7cdc1d30db698fd08d1219a7cab5d7
Muzahidul Parvez <81441097+robinusa21@users.noreply.github.com>
2024-01-13 00:38:12 +0600

    me to

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'dark': '#181818',
+        'orange': '#ff4a17',
+      }
+    },
+  },
+  plugins: [],
+}
+
